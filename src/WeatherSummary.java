f17333d8b416b4ab473d8188d9925db0a754a1c1
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-09 16:57:56 -0700

    added average for temps. Some untracked files are from switching to IntelliJ then back to VSCode.

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -20,27 +20,42 @@ public class WeatherSummary {
         // Implement this method!
         // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
         Scanner scanner = new Scanner(System.in); //adding scanner
-        /** step 2 
+
+        /*
+          WAVE 2
         while(scanner.hasNextDouble()) { //makes sure code has hasNextDouble
             double temps = scanner.nextDouble(); //makes sure code has nextDouble
             System.out.println(temps);
         }
-            **/
-            double min = Double.POSITIVE_INFINITY;
-            double max = Double.NEGATIVE_INFINITY;
+            */
+
+        if (!scanner.hasNextDouble()) {
+            System.out.println("N/A");
+            scanner.close();
+            return;
+        }
+            double temps = scanner.nextDouble();
+            double min = temps;
+            double max = temps;
+            double sum = temps;
+            int count = 1;
+
             while(scanner.hasNextDouble()) {
-                double temps = scanner.nextDouble();
-                if (temps < min) {
-                    min = temps;
-                }
-                if (temps > max) {
-                    max = temps;
-                }
-                }
+                temps = scanner.nextDouble();
+                sum += temps;
+                count++;
+
+                if (temps < min)  min = temps;
+                if (temps > max)  max = temps;
+                
+            }
         scanner.close();
+        double average = sum/count;
 
         System.out.println("Max: " + max);
         System.out.println("Min: " + min);
+        System.out.println("Average: " + average);
     }
 }
+    
 
