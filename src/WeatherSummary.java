0958263a3dbcfd62a227bde4eabe3e1e5d5bd664
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-04-09 17:41:38 -0700

    Created the Average method and printed out Max, Min, Avg. I also put them in their own seperate methods to clean up, while adding and moving a few comments.

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/*
+    Name: Brendan Villaraza
+    Date: 4/9/2025
+    Assignment: weather checker
+ */
 public class WeatherSummary {
     /**
      * Reads newline-delimted temperatures from System.in and prints summary
@@ -21,6 +25,7 @@ public class WeatherSummary {
     public static void main(String[] args) {
         // Implement this method!
         // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
+        //javac src/WeatherSummary.java && java -cp src WeatherSummary < temps
         Scanner scanner = new Scanner(System.in);
 
         // create the list to put the temp values in
@@ -30,6 +35,28 @@ public class WeatherSummary {
             tempList.add(temp);
             // System.out.println(scanner.nextDouble());
         }
+
+        //calling the methods
+        printMinAndMax(tempList);
+        printAvg(tempList);
+      
+        
+    }
+
+    //moved into methods just to clean up a bit
+    public static void printAvg(ArrayList<Double> tempList){
+          //Create the print for the average
+        int count = tempList.size();
+        double sum = 0;
+        for(double temp: tempList){
+            sum += temp;
+        }
+        double avg = sum/count;
+
+        System.out.println("Avg: " + avg);
+    }
+
+    public static void printMinAndMax(ArrayList<Double> tempList){
         //Creating a bubble sort so I can sort the list
         for (int i = 0; i < tempList.size() - 1; i++) {
             for (int j = 0; j < tempList.size() - i - 1; j++) {
@@ -40,12 +67,12 @@ public class WeatherSummary {
                 }
             }
         }
-        
+      
         System.out.println("Max: " + tempList.get(0));
 
         // Source: StackOverflow - "How to get the last value of an ArrayList"
         // https://stackoverflow.com/questions/687833/how-to-get-the-last-value-of-an-arraylist
         System.out.println("Min: " + tempList.get(tempList.size()-1));
-    }
-    //javac src/WeatherSummary.java && java -cp src WeatherSummary < temps
+    
+  }
 }
